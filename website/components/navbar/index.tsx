e460d2ea1c0e2a02a1e8f9dd6cd18b7ed2da4f9d
yanrie1201 <vincentyanrie3@gmail.com>
2022-06-21 21:13:15 +0700

    update component

--- website/components/navbar/index.tsx
+++ website/components/navbar/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-no-duplicate-props */
-import { AccountName, NavbarPage, TitlePage } from '../typography'
+import { TextContent, NavbarPage, TitlePage } from '../typography'
 import styles from '../../styles/navbar.module.css'
 import { Avatar, Box, Stack } from '@mui/material'
 import logo from '../../public/images/white_logo-removebg-preview.png'
--- website/components/typography/index.tsx
+++ website/components/typography/index.tsx
@@ -27,19 +27,15 @@ export const TitlePage = styled(Typography)<{
   text-align: ${(props) => props.textalign || 'left'};
 `
 
-export const AccountName = styled(Typography)<{
+export const TextContent = styled(Typography)<{
   color?: string
+  textalign?: string
+  fsize?: string
+  fontWeight?: string
 }>`
-  font-family: Quicksand;
-  font-size: 15px;
-  font-weight: 500;
+  font-family: Quicksand, sans-serif;
+  font-size: ${(props) => props.fsize || '22px'};
+  font-weight: ${(props) => props.fontWeight || '500'};
   color: ${(props) => props.color || 'black'};
-  margin-right: 15px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  @media screen and (max-width: 425px) {
-    display: none;
-  }
+  text-align: ${(props) => props.textalign || 'left'};
 `
