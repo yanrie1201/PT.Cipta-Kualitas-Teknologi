468bb60fb236ccad98676b8c2595be0cb898d2c5
Andrew <xxxandas@gmail.com>
2022-06-22 15:32:57 +0700

    Update Logo to SVG

--- website/components/logo/index.tsx
+++ website/components/logo/index.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable react/jsx-no-duplicate-props */
-import { NavbarPage, TitlePage } from '../typography'
-import styles from '../../styles/navbar.module.css'
-import { Avatar, Box, Stack } from '@mui/material'
-import logo from '../../public/images/Logo/0.5x/Asset 7.png'
-import Image from 'next/image'
-import { style } from '@mui/system'
+import { NavbarPage, TitlePage } from "../typography";
+import styles from "../../styles/navbar.module.css";
+import { Avatar, Box, Stack } from "@mui/material";
+import logo from "../../public/images/Logo/SVG/Asset 7.svg";
+import Image from "next/image";
+import { style } from "@mui/system";
 
 interface LogoProps {
-  width: string
-  height: string
+  width: string;
+  height: string;
 }
 
 const Logo = ({ width, height }: LogoProps) => {
@@ -22,7 +22,7 @@ const Logo = ({ width, height }: LogoProps) => {
         alt="Logo"
       />
     </>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
