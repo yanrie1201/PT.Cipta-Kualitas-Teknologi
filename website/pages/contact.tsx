2979bc61719e7e4685fb263d30640acf33ad997c
Vincent <vincentyanrie3@gmail.com>
2022-06-28 13:04:15 +0700

    adding contact page update

--- website/pages/contact.tsx
+++ website/pages/contact.tsx
@@ -48,24 +48,28 @@ const About: NextPage = () => (
 
     <Navbar
       setBackgroundColor="var(--white)"
-      setPosition="absolute"
+      setPosition="relative"
       setLogoFilter="none"
       setTextColor="var(--black)"
       setShadow="0 0 15px 0px rgba(0, 0, 0, 0.5)"
     />
     <main className={styles.contact}>
       <section className={styles.header}>
-        <Container maxWidth="xl">
-          <Grid container justifyContent="left" alignItems="center">
-            <Grid md={7}>
-              <Paper sx={{ padding: '10px 10px' }}>
+        <div className={styles.backgroundheader}>
+          <Container maxWidth="xl">
+            <Grid container alignItems="center" className={styles.form}>
+              <Grid
+                md={7}
+                sx={{ backgroundColor: '#ffff' }}
+                className={styles.form1}
+              >
                 <FooterText color="#000" fsize="30px" textalign="center">
                   Contact Us
                 </FooterText>
                 <FooterText color="#000" fsize="15px">
                   *You can contact us using form that we already provided
                 </FooterText>
-                <Stack direction="column" spacing={2}>
+                <Stack direction="column" spacing={3}>
                   <CssTextField type="text" label="Full Name" />
                   <CssTextField type="text" label="Email" />
                   <CssTextField type="text" label="Phone Number" />
@@ -85,11 +89,50 @@ const About: NextPage = () => (
                     Submit
                   </HomeButton>
                 </Box>
-              </Paper>
+              </Grid>
+              <Grid md={5} className={styles.contentright}>
+                <Stack direction="column" sx={{ paddingBottom: '20px' }}>
+                  <FooterText
+                    fontWeight="600"
+                    fsize="25px"
+                    mbottom="2px"
+                    sx={{ textDecoration: 'underline' }}
+                  >
+                    Contact Information
+                  </FooterText>
+                  <FooterText fsize="15px" mbottom="0px">
+                    PT. Cipta Kualitas Teknologi
+                  </FooterText>
+                  <FooterText fsize="15px" mbottom="0px">
+                    Mayapada Tower, 11th Floor
+                  </FooterText>
+                  <FooterText fsize="15px" mbottom="0px">
+                    Jl. Jend. Sudirman, Kav. 28,
+                  </FooterText>
+                  <FooterText fsize="15px" mbottom="0px">
+                    Jakarta 12920 - INDONESIA
+                  </FooterText>
+                  <FooterText fsize="15px" mbottom="0px">
+                    Phone: +62 62 5289 7480
+                  </FooterText>
+                  <FooterText fsize="15px" mbottom="0px">
+                    Facsimile: +62 21 5289 7399
+                  </FooterText>
+                  <FooterText fsize="15px" mbottom="0px">
+                    Email: Christian.kangean@Ciptakualitas.co.id
+                  </FooterText>
+                </Stack>
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.2836146676937!2d106.65807671535181!3d-6.226287462712871!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69fb9f858ae757%3A0x53dfe445d4dfb6d!2sPT.%20Cipta%20Kualitas%20Teknologi!5e0!3m2!1sen!2sid!4v1656393314465!5m2!1sen!2sid"
+                  width="500"
+                  height="450"
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+              </Grid>
             </Grid>
-            <Grid md={5}></Grid>
-          </Grid>
-        </Container>
+          </Container>
+        </div>
       </section>
     </main>
     <Footer />
